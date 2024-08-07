c3f5766af16cba9860bb2874fa10f27324b8eddd
Sahaquiel17 <150754153+Sahaquiel17@users.noreply.github.com>
2024-08-07 13:15:25 -0600

    Update gessucursales.js

--- modulos/modulosProductos_omar/gessucursales.js
+++ modulos/modulosProductos_omar/gessucursales.js
@@ -1,6 +1,6 @@
 let obj = []; // Arreglo que se llenará de objetos JSON
 let indexProductosSeleccionados; // Índice del arreglo
-let path = "http://localhost:8080/Proyecto1/modulos/modulosProductos_omar/";
+let path = "modulos/modulosProductos_omar/";
 
 fetch(path + "datoSucursal.json")
         .then((response) => response.json())
